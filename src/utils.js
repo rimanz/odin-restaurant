e71e475a754b1d13c452c8045ec4ca1c8da9a938
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-02-18 23:04:25 +0600

    Add JSDoc comments to the functions in src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
+/**
+ * Create and return a HTMLElement.
+ * @param {object} options - Options to configure the element.
+ * @param {string} [options.tag='div'] - HTML Element's name(tag). Defaults to a 'div' tag.
+ * @param {string} options.id - Element's id attribute.
+ * @param {string} options.classNames - Element's space separated class names.
+ * @param {string} options.textContent - Texts inside the element.
+ * @param {HTMLElement} [options.parent=document.body] - Element's direct parent node. Defaults to the HTMLBodyElement.
+ * @returns {object} The HTMLElement.
+ */
 export function createNode(options) {
   const node = document.createElement(options.tag || "div");
   if (options.id) node.setAttribute("id", options.id);
@@ -18,6 +28,16 @@ export function createNode(options) {
   return node;
 }
 
+/**
+ * Create a HTMLElement with a wrapper inside it and return wrapper Element.
+ * @param {object} options - Options to configure the element.
+ * @param {string} [options.tag='section'] - HTML Element's name(tag). Defaults to a 'section' tag.
+ * @param {string} options.id - Element's id attribute.
+ * @param {string} options.classNames - Element's space separated class names.
+ * @param {string} options.textContent - Texts inside the element.
+ * @param {HTMLElement} [options.parent=document.body] - Element's direct parent node. Defaults to the HTMLBodyElement.
+ * @returns {object} The HTMLElement.
+ */
 export function sectionBuilder(options) {
   const section = createNode({ ...options, tag: options.tag || "section" });
   const container = createNode({ classNames: "container", parent: section });
@@ -31,5 +51,7 @@ export function sectionBuilder(options) {
     });
   }
 
+  // Returns the container inside the section, not the section itself!
+  // It makes it easier to refer container to put things inside it.
   return container;
 }
