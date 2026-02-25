b78727edf79c62cfa4e2035437575ffe378e601d
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-02-25 10:59:10 +0600

    Add scripts to streamline dev experience
    - Split webpack config into common, dev, and prod files
    - Add npm scripts for development and production builds
    - Update README with new scripts and instructions

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "node:path";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
